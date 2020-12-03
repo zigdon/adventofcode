b4901f3af1259142defdfd03fc0342703d4f9ab8
Dan Boger <dan@peeron.com>
2020-12-02 23:15:56 -0800

    3/1

---
+++ 2020/3/3.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type trees struct {
+	data          map[int]map[int]bool
+	width, height int
+}
+
+func (t *trees) init(data string) {
+	t.data = make(map[int]map[int]bool)
+	t.width = 0
+	t.height = 0
+	t.load(data)
+}
+
+func (t *trees) get(x, y int) bool {
+	if y >= t.height {
+		return false
+	}
+	return t.data[y][x%t.width]
+}
+
+func (t *trees) load(data string) {
+	lines := strings.Split(string(data), "\n")
+	for l, ldata := range lines {
+		line := strings.TrimSpace(ldata)
+		if len(line) == 0 {
+			continue
+		}
+		if t.data[l] == nil {
+			t.data[l] = make(map[int]bool)
+			if l >= t.height {
+				t.height = l + 1
+			}
+		}
+		for c, r := range line {
+			t.data[l][c] = r == '#'
+			if c >= t.width {
+				t.width = c + 1
+			}
+		}
+	}
+}
+
+func (t *trees) readInput(path string) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	t.init(string(data))
+	return nil
+}
+
+func (t *trees) countTrees(right, down int) int {
+	var x, y, res int
+	for y < t.height {
+		if t.get(x, y) {
+			res = res + 1
+		}
+		x = x + right
+		y = y + down
+	}
+	return res
+}
+
+func main() {
+	input := os.Args[1]
+	terrain := &trees{}
+	err := terrain.readInput(input)
+	if err != nil {
+		log.Fatalf("Can't read input: %v", err)
+	}
+
+	count := terrain.countTrees(3, 1)
+	fmt.Println(count)
+}
---
+++ 2020/3/3_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"testing"
+)
+
+const (
+	samplePath = "sample.txt"
+)
+
+func TestReadInput(t *testing.T) {
+	terrain := &trees{}
+	err := terrain.readInput(samplePath)
+	if err != nil {
+		t.Fatalf("Error reading input: %v", err)
+	}
+
+	var trees = map[int]map[int]bool{
+		0:  {2: true, 3: true},
+		1:  {0: true, 4: true, 8: true},
+		2:  {1: true, 6: true, 9: true},
+		3:  {2: true, 4: true, 8: true, 10: true},
+		4:  {1: true, 5: true, 6: true, 9: true},
+		5:  {2: true, 4: true, 5: true},
+		6:  {1: true, 3: true, 5: true, 10: true},
+		7:  {1: true, 10: true},
+		8:  {0: true, 2: true, 3: true, 7: true},
+		9:  {0: true, 4: true, 5: true, 10: true},
+		10: {1: true, 4: true, 8: true, 10: true},
+	}
+	for y, line := range terrain.data {
+		for x, tree := range line {
+			if tree != trees[y][x] {
+				t.Errorf("Bad tree at %d, %d: want %v got %v", x, y, trees[y][x], tree)
+			}
+		}
+	}
+}
+
+func TestGet(t *testing.T) {
+	data := trees{}
+	data.init("#.#\n..#\n")
+	tests := []struct {
+		desc string
+		x, y int
+		want bool
+	}{
+		{"simple tree", 0, 0, true},
+		{"simple open", 0, 1, false},
+		{"past", 1, 2, false},
+		{"repeat tree", 3, 0, true},
+		{"repeat open", 3, 1, false},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := data.get(tc.x, tc.y)
+			if got != tc.want {
+				t.Errorf("Tree mismatch at %d,%d: want %v got %v", tc.x, tc.y, tc.want, got)
+			}
+		})
+	}
+}
+
+func TestCount(t *testing.T) {
+	terrain := &trees{}
+	err := terrain.readInput(samplePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	found := terrain.countTrees(3, 1)
+	if found != 7 {
+		t.Errorf("Ran into the wrong number of trees: expacted 7, found %d", found)
+	}
+}
