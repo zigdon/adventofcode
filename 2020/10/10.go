39e6860ae4927cb79b63c9c221bfe3af03109c02
Dan Boger <dan@peeron.com>
2020-12-10 00:44:42 -0800

    10/2 so indeed. the tests are broken, but the answer was correct.

--- 2020/10/10.go
+++ 2020/10/10.go
@@ -10,6 +10,66 @@ import (
 	"strings"
 )
 
+// Given a sorted chain of adapters, how ways could they be combined
+func countChains(adapters []int) int {
+	if len(adapters) <= 2 {
+		return 1
+	}
+	start := adapters[0]
+	count := 0
+	count = count + countChains(adapters[1:])
+
+	if len(adapters) >= 3 && adapters[2]-start <= 3 {
+		count = count + countChains(adapters[2:])
+	}
+
+	if len(adapters) >= 4 && adapters[3]-start <= 3 {
+		count = count + countChains(adapters[3:])
+	}
+
+	return count
+}
+
+func sectionAdapters(adapters []int) [][]int {
+	res := [][]int{}
+	sort.Ints(adapters)
+	section := []int{}
+	last := adapters[0]
+	for _, n := range adapters[1:] {
+		if n-last < 3 {
+			if len(section) == 0 {
+				section = append(section, last)
+			}
+			section = append(section, n)
+		} else {
+			if len(section) > 1 {
+				res = append(res, section)
+			}
+			section = []int{}
+		}
+		last = n
+	}
+
+	if len(section) > 0 {
+		res = append(res, section)
+	}
+
+	return res
+}
+
+func buildChain(adapters []int) int {
+	count := 1
+	sort.Ints(adapters)
+
+	sections := sectionAdapters(adapters)
+	for _, s := range sections {
+		c := countChains(s)
+		count = count * c
+	}
+
+	return count
+}
+
 func getDist(adapters []int) map[int]int {
 	sort.Ints(adapters)
 	dist := make(map[int]int)
@@ -45,5 +105,6 @@ func main() {
 	dist := getDist(adapters)
 	fmt.Printf("%v\n", dist)
 	fmt.Println(dist[1] * dist[3])
-
+	adapters = append(adapters, 0)
+	fmt.Printf("chains: %d", buildChain(adapters))
 }
--- 2020/10/10_test.go
+++ 2020/10/10_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -13,6 +14,79 @@ func sample1() []int {
 func sample2() []int {
 	return []int{28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49,
 		45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3}
+}
+
+func TestSectionAdapters(t *testing.T) {
+	tests := []struct {
+		data []int
+		want [][]int
+	}{
+		{
+			data: sample1(),
+			want: [][]int{
+				{4, 5, 6, 7},
+				{10, 11, 12},
+				{15, 16},
+			},
+		},
+		{
+			data: sample2(),
+			want: [][]int{
+				{1, 2, 3, 4},
+				{7, 8, 9, 10, 11},
+				{17, 18, 19, 20},
+				{23, 24, 25},
+				{31, 32, 33, 34, 35},
+				{38, 39},
+				{45, 46, 47, 48, 49},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		got := sectionAdapters(tc.data)
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("Bad sections:\n%s", diff)
+		}
+	}
+}
+
+func TestCountChains(t *testing.T) {
+	tests := []struct {
+		data []int
+		want int
+	}{
+		{data: []int{1, 2, 3, 4}, want: 4},
+		{data: sample1(), want: 8},
+		{data: sample2(), want: 19208},
+	}
+
+	for _, tc := range tests {
+		sort.Ints(tc.data)
+		got := countChains(tc.data)
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("bad count: %s", diff)
+		}
+	}
+}
+
+func TestBuildChains(t *testing.T) {
+	tests := []struct {
+		data []int
+		want int
+	}{
+		{data: sample1(), want: 8},
+		{data: sample2(), want: 19208},
+	}
+
+	for _, tc := range tests {
+		tc.data = append(tc.data, 0)
+		sort.Ints(tc.data)
+		got := buildChain(tc.data)
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("bad build: %s", diff)
+		}
+	}
 }
 
 func TestGetDist(t *testing.T) {
