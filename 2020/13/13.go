3f2eecfb11c1328bd8888d5e2bdd08e3ef4ff364
Dan Boger <dan@peeron.com>
2020-12-16 22:03:38 -0800

    13/2 Brute force - it works in theory, but ain't no one got time for that

--- 2020/13/13.go
+++ 2020/13/13.go
@@ -9,14 +9,14 @@ import (
 	"strings"
 )
 
-func parseSchedule(lines []string) (int, map[int]bool) {
+func parseSchedule(lines []string) (int, map[int]int) {
 	var start int
-	buses := make(map[int]bool)
+	buses := make(map[int]int)
 	start, err := strconv.Atoi(lines[0])
 	if err != nil {
 		log.Fatalf("parse error for %q: %v", lines[0], err)
 	}
-	for _, bus := range strings.Split(lines[1], ",") {
+	for n, bus := range strings.Split(lines[1], ",") {
 		if bus == "x" {
 			continue
 		}
@@ -25,13 +25,13 @@ func parseSchedule(lines []string) (int, map[int]bool) {
 			log.Fatalf("skipping bus %q: %v", bus, err)
 			continue
 		}
-		buses[no] = true
+		buses[no] = n
 	}
 
 	return start, buses
 }
 
-func nextBus(time int, buses map[int]bool) (int, int) {
+func nextBus(time int, buses map[int]int) (int, int) {
 	var line int
 	wait := -1
 	for b := range buses {
@@ -45,6 +45,43 @@ func nextBus(time int, buses map[int]bool) (int, int) {
 	return wait, line
 }
 
+func validateTime(ts int, sched map[int]int) bool {
+	for line, off := range sched {
+		if (ts+off)%line != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func findSequence(start, end int, sched map[int]int, verbose bool) int {
+	var max, offset int
+	for line, ord := range sched {
+		fmt.Printf("%d:%d, ", ord, line)
+		if line > max {
+			max = line
+			offset = ord
+		}
+	}
+	fmt.Println()
+
+	cur := start + (start % max) + offset
+	fmt.Printf("starting at %d\n", cur)
+	for {
+		if validateTime(cur, sched) {
+			fmt.Println()
+			return cur
+		}
+		cur += max
+		if end > 0 && cur > end {
+			return -1
+		}
+		if verbose {
+			fmt.Printf("%d\r", cur)
+		}
+	}
+}
+
 func main() {
 	input := os.Args[1]
 	data, err := ioutil.ReadFile(input)
@@ -54,5 +91,7 @@ func main() {
 
 	start, sched := parseSchedule(strings.Split(string(data), "\n"))
 	wait, line := nextBus(start, sched)
-	fmt.Printf("%d minues until %d arrives => %d", wait, line, wait*line)
+	fmt.Printf("%d minues until %d arrives => %d\n", wait, line, wait*line)
+
+	fmt.Println("magic moment: ", findSequence(100000000000000, 0, sched, true))
 }
--- 2020/13/13_test.go
+++ 2020/13/13_test.go
@@ -16,12 +16,12 @@ func sample() []string {
 func TestParseSchedule(t *testing.T) {
 	gotS, gotB := parseSchedule(sample())
 
-	want := map[int]bool{
-		7:  true,
-		13: true,
-		59: true,
-		31: true,
-		19: true,
+	want := map[int]int{
+		7:  0,
+		13: 1,
+		59: 4,
+		31: 6,
+		19: 7,
 	}
 	if gotS != 939 {
 		t.Errorf("bad start: %d", gotS)
@@ -39,5 +39,50 @@ func TestNextBus(t *testing.T) {
 	}
 	if line != 59 {
 		t.Errorf("bad line: %d", line)
+	}
+}
+
+func TestValidateTime(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{input: sample()[1], want: 1068781},
+		{input: "17,x,13,19", want: 3417},
+		{input: "67,7,59,61", want: 754018},
+		{input: "67,x,7,59,61", want: 779210},
+		{input: "67,7,x,59,61", want: 1261476},
+		{input: "1789,37,47,1889", want: 1202161486},
+	}
+
+	for i, tc := range tests {
+		_, sched := parseSchedule([]string{"0", tc.input})
+		got := validateTime(tc.want, sched)
+		if !got {
+			t.Errorf("bad validation of %d #%d", tc.want, i)
+		}
+	}
+}
+
+func TestFindSequence(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+		max   int
+	}{
+		{input: sample()[1], want: 1068781, max: 59},
+		{input: "17,x,13,19", want: 3417, max: 19},
+		{input: "67,7,59,61", want: 754018, max: 67},
+		{input: "67,x,7,59,61", want: 779210, max: 67},
+		{input: "67,7,x,59,61", want: 1261476, max: 67},
+		{input: "1789,37,47,1889", want: 1202161486, max: 1889},
+	}
+
+	for i, tc := range tests {
+		_, sched := parseSchedule([]string{"0", tc.input})
+		got := findSequence(tc.want-2*tc.max, tc.want+2000, sched, false)
+		if tc.want != got {
+			t.Errorf("bad sequence found for #%d: want %d, got %d", i, tc.want, got)
+		}
 	}
 }
