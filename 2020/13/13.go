088749ef9e1d50734c39bdcafd8fcc375094f03b
Dan Boger <dan@peeron.com>
2020-12-17 15:24:05 -0800

    13/2 Well, some googling for the math I need, then lots of headbanging

--- 2020/13/13.go
+++ 2020/13/13.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -45,39 +46,51 @@ func nextBus(time int, buses map[int]int) (int, int) {
 	return wait, line
 }
 
-func validateTime(ts int, sched map[int]int) bool {
-	for line, off := range sched {
-		if (ts+off)%line != 0 {
-			return false
+// {lineno: offset}
+func findSequence(start int, sched map[int]int) int {
+	fmt.Printf("sched: %v\n", sched)
+	var keys []int
+	pairs := make(map[int]int)
+	for line, offset := range sched {
+		keys = append(keys, line)
+		if offset > 0 {
+			pairs[line] = line - offset
+			for pairs[line] < 0 {
+				pairs[line] += line
+			}
+		} else {
+			pairs[line] = 0
 		}
 	}
-	return true
-}
-
-func findSequence(start, end int, sched map[int]int, verbose bool) int {
-	var max, offset int
-	for line, ord := range sched {
-		fmt.Printf("%d:%d, ", ord, line)
-		if line > max {
-			max = line
-			offset = ord
-		}
+	sort.Ints(keys)
+	var rev []int // line numbers, descending order
+	for _, line := range keys {
+		rev = append([]int{line}, rev...)
 	}
-	fmt.Println()
+	fmt.Printf("pairs: %v\n", pairs)
 
-	cur := start + (start % max) + offset
-	fmt.Printf("starting at %d\n", cur)
+	// https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+	idx := 1
+	var cur int
+	if start == 0 {
+		cur = pairs[rev[0]]
+	} else {
+		cur = start - (start % rev[0]) + pairs[rev[0]]
+		fmt.Printf("starting at %d\n", cur)
+	}
+
+	step := rev[0]
 	for {
-		if validateTime(cur, sched) {
-			fmt.Println()
+		fmt.Printf("%d  %d\r", step, cur)
+		if cur%rev[idx] != pairs[rev[idx]] {
+			cur += step
+			continue
+		}
+		step *= rev[idx]
+		fmt.Printf("%d > %v %d %v\n", cur, rev[:idx], rev[idx], rev[idx+1:])
+		idx++
+		if idx >= len(rev) {
 			return cur
-		}
-		cur += max
-		if end > 0 && cur > end {
-			return -1
-		}
-		if verbose {
-			fmt.Printf("%d\r", cur)
 		}
 	}
 }
@@ -93,5 +106,5 @@ func main() {
 	wait, line := nextBus(start, sched)
 	fmt.Printf("%d minues until %d arrives => %d\n", wait, line, wait*line)
 
-	fmt.Println("magic moment: ", findSequence(100000000000000, 0, sched, true))
+	fmt.Println("magic moment: ", findSequence(100000000000000, sched))
 }
--- 2020/13/13_test.go
+++ 2020/13/13_test.go
@@ -42,7 +42,7 @@ func TestNextBus(t *testing.T) {
 	}
 }
 
-func TestValidateTime(t *testing.T) {
+func TestFindSequence(t *testing.T) {
 	tests := []struct {
 		input string
 		want  int
@@ -57,30 +57,7 @@ func TestValidateTime(t *testing.T) {
 
 	for i, tc := range tests {
 		_, sched := parseSchedule([]string{"0", tc.input})
-		got := validateTime(tc.want, sched)
-		if !got {
-			t.Errorf("bad validation of %d #%d", tc.want, i)
-		}
-	}
-}
-
-func TestFindSequence(t *testing.T) {
-	tests := []struct {
-		input string
-		want  int
-		max   int
-	}{
-		{input: sample()[1], want: 1068781, max: 59},
-		{input: "17,x,13,19", want: 3417, max: 19},
-		{input: "67,7,59,61", want: 754018, max: 67},
-		{input: "67,x,7,59,61", want: 779210, max: 67},
-		{input: "67,7,x,59,61", want: 1261476, max: 67},
-		{input: "1789,37,47,1889", want: 1202161486, max: 1889},
-	}
-
-	for i, tc := range tests {
-		_, sched := parseSchedule([]string{"0", tc.input})
-		got := findSequence(tc.want-2*tc.max, tc.want+2000, sched, false)
+		got := findSequence(0, sched)
 		if tc.want != got {
 			t.Errorf("bad sequence found for #%d: want %d, got %d", i, tc.want, got)
 		}
