4b88587770ff0f6a0d959f84a49d63648c2cd46f
Dan Boger <dan@peeron.com>
2020-12-19 21:35:58 -0800

    15/1 So the later tests fail, but the answer is correct.

---
+++ 2020/15/15.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type ent struct {
+	last int
+	init bool
+}
+
+func getSequence(init []int, pos int) int {
+	mem := make(map[int]int)
+	var seq []int
+	var last, next int
+	for step := 0; step < len(init); step++ {
+		next = init[step]
+		mem[last] = step
+		last = next
+		seq = append(seq, next)
+	}
+	for step := len(init); step < pos; step++ {
+		// log.Printf("step=%d", step)
+		if seen, ok := mem[last]; ok {
+			next = step - seen
+			// log.Printf("%d was last seen on %d, so next=%d", last, seen, next)
+		} else {
+			next = 0
+		}
+		mem[last] = step
+		last = next
+		seq = append(seq, next)
+		// log.Printf("mem: %v", mem)
+	}
+	// log.Printf("%v", seq)
+
+	return last
+}
+
+func main() {
+	input := []int{}
+	for _, s := range strings.Split(os.Args[1], ",") {
+		n, _ := strconv.Atoi(s)
+		input = append(input, n)
+	}
+	fmt.Println(input)
+	fmt.Println(getSequence(input, 2020))
+}
---
+++ 2020/15/15_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestGetSequence(t *testing.T) {
+	tests := []struct {
+		starting []int
+		want     int
+		pos      int
+	}{
+		{starting: []int{0, 3, 6}, want: 436},
+		{starting: []int{0, 3, 6}, pos: 4, want: 0},
+		{starting: []int{0, 3, 6}, pos: 5, want: 3},
+		{starting: []int{0, 3, 6}, pos: 6, want: 3},
+		{starting: []int{0, 3, 6}, pos: 7, want: 1},
+		{starting: []int{0, 3, 6}, pos: 8, want: 0},
+		{starting: []int{0, 3, 6}, pos: 9, want: 4},
+		{starting: []int{0, 3, 6}, pos: 10, want: 0},
+		{starting: []int{1, 3, 2}, want: 1},
+		{starting: []int{2, 1, 3}, want: 10},
+		{starting: []int{1, 2, 3}, want: 27},
+		{starting: []int{2, 3, 1}, want: 78},
+		{starting: []int{3, 2, 1}, want: 438},
+		{starting: []int{3, 1, 2}, want: 1836},
+	}
+
+	for i, tc := range tests {
+		pos := tc.pos
+		if pos == 0 {
+			pos = 2020
+		}
+		got := getSequence(tc.starting, pos)
+		if got != tc.want {
+			t.Errorf("bad sequence for test #%d, position %d: want %d got %d", i, pos, tc.want, got)
+		}
+	}
+}
