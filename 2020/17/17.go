212d5093c9e0ec44739d7ca7028d1bddd337f59d
Dan Boger <dan@peeron.com>
2020-12-20 17:31:29 -0800

    17/1 3d game of life - starting to pass sample tests

---
+++ 2020/17/17.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type coord struct {
+	X, Y, Z int
+}
+
+type space struct {
+	Data        map[coord]bool
+	Lx, Ly, Lz  int
+	Hx, Hy, Hz  int
+	Initialized bool
+}
+
+func newSpace() *space {
+	return &space{Data: make(map[coord]bool)}
+}
+
+func (s *space) set(p coord, v bool) {
+	if !s.Initialized {
+		s.Hx = p.X
+		s.Lx = p.X
+		s.Hy = p.Y
+		s.Ly = p.Y
+		s.Hz = p.Z
+		s.Lz = p.Z
+		s.Initialized = true
+	} else if v {
+		if p.X > s.Hx {
+			s.Hx = p.X
+		}
+		if p.X < s.Lx {
+			s.Lx = p.X
+		}
+		if p.Y > s.Hy {
+			s.Hy = p.Y
+		}
+		if p.Y < s.Ly {
+			s.Ly = p.Y
+		}
+		if p.Z > s.Hz {
+			s.Hz = p.Z
+		}
+		if p.Z < s.Lz {
+			s.Lz = p.Z
+		}
+	}
+
+	s.Data[p] = v
+}
+
+func (s *space) get(p coord) bool {
+	return s.Data[p]
+}
+
+func (s *space) getTrue() []coord {
+	var res []coord
+	for k, v := range s.Data {
+		if v {
+			res = append(res, k)
+		}
+	}
+
+	return res
+}
+
+func (s *space) loadSlice(slice string, z int) {
+	for y, line := range strings.Split(slice, "\n") {
+		for x, c := range line {
+			if c == '#' {
+				s.set(coord{x, y, z}, true)
+			}
+		}
+	}
+}
+
+func (s *space) count(p coord) int {
+	res := 0
+	for _, x := range []int{-1, 0, 1} {
+		for _, y := range []int{-1, 0, 1} {
+			for _, z := range []int{-1, 0, 1} {
+				if x == 0 && y == 0 && z == 0 {
+					continue
+				}
+				if s.get(coord{p.X + x, p.Y + y, p.Z + z}) {
+					res++
+				}
+			}
+		}
+	}
+	return res
+}
+
+func (s *space) dump() string {
+	var res string
+	for z := s.Lz; z <= s.Hz; z++ {
+		res += fmt.Sprintf("Layer: %d\n", z)
+		for y := s.Ly; y <= s.Hy; y++ {
+			for x := s.Lx; x <= s.Hx; x++ {
+				if s.get(coord{x, y, z}) {
+					res += "#"
+				} else {
+					res += "."
+				}
+			}
+			res += "\n"
+		}
+	}
+
+	return res
+}
+
+func (s *space) evolve() {
+	next := newSpace()
+	for x := s.Lx - 1; x <= s.Hx+1; x++ {
+		for y := s.Ly - 1; y <= s.Hy+1; y++ {
+			for z := s.Lz - 1; z <= s.Hz+1; z++ {
+				p := coord{x, y, z}
+				cnt := s.count(p)
+				if s.get(p) {
+					if cnt == 2 || cnt == 3 {
+						next.set(p, true)
+					}
+				} else {
+					if cnt == 3 {
+						next.set(p, true)
+					}
+				}
+			}
+		}
+	}
+	s.Data = next.Data
+	s.Lx, s.Ly, s.Lz = next.Lx, next.Ly, next.Lz
+	s.Hx, s.Hy, s.Hz = next.Hx, next.Hy, next.Hz
+}
+
+func main() {
+	input := os.Args[1]
+	_, err := ioutil.ReadFile(input)
+	if err != nil {
+		log.Fatalf("can't read %q: %v", input, err)
+	}
+}
---
+++ 2020/17/17_test.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type point struct {
+	xyz coord
+	val bool
+}
+
+func coordSort(data []coord) func(int, int) bool {
+	return func(j, k int) bool {
+		a := data[j]
+		b := data[k]
+		if a.X < b.X {
+			return false
+		}
+		if a.X > b.X {
+			return true
+		}
+		if a.Y < b.Y {
+			return false
+		}
+		if a.Y > b.Y {
+			return true
+		}
+		if a.Z < b.Z {
+			return false
+		}
+		if a.Z > b.Z {
+			return true
+		}
+		return false
+	}
+}
+
+func TestGetSet(t *testing.T) {
+	tests := []struct {
+		pts  []point
+		want []coord
+	}{
+		{
+			pts: []point{
+				{coord{1, 1, 1}, true},
+				{coord{1, 2, 1}, true},
+				{coord{2, -1, 2}, true},
+			},
+			want: []coord{coord{1, 1, 1}, coord{1, 2, 1}, coord{2, -1, 2}},
+		},
+		{
+			pts: []point{
+				{coord{1, 1, 1}, true},
+				{coord{1, 1, 1}, false},
+				{coord{2, -1, 2}, false},
+				{coord{2, -1, 2}, true},
+			},
+			want: []coord{coord{2, -1, 2}},
+		},
+	}
+
+	for _, tc := range tests {
+		s := newSpace()
+		for _, p := range tc.pts {
+			s.set(p.xyz, p.val)
+		}
+
+		got := s.getTrue()
+		sort.Slice(got, coordSort(got))
+		sort.Slice(tc.want, coordSort(tc.want))
+
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("bad values: %s", diff)
+		}
+	}
+}
+
+func TestLoadSlice(t *testing.T) {
+	data := strings.Join([]string{".#.", "..#", "###"}, "\n")
+	s := newSpace()
+	s.loadSlice(data, 2)
+	got := s.getTrue()
+	want := []coord{{1, 0, 2}, {2, 1, 2}, {0, 2, 2}, {1, 2, 2}, {2, 2, 2}}
+	sort.Slice(got, coordSort(got))
+	sort.Slice(want, coordSort(want))
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("bad slicing: %s", diff)
+	}
+}
+
+func TestCount(t *testing.T) {
+	s := newSpace()
+	data := strings.Join([]string{".#.", "..#", "###"}, "\n")
+	s.loadSlice(data, 0)
+	data = strings.Join([]string{"#.#"}, "\n")
+	s.loadSlice(data, 1)
+	tests := []struct {
+		x, y, z int
+		want    int
+	}{
+		{0, 0, 0, 2},
+		{1, 1, 0, 7},
+		{0, 0, 2, 1},
+	}
+
+	for _, tc := range tests {
+		got := s.count(coord{tc.x, tc.y, tc.z})
+		if got != tc.want {
+			t.Errorf("bad count at %d,%d,%d: want %d, got %d", tc.x, tc.y, tc.z, tc.want, got)
+		}
+	}
+}
+
+func TestEvolve(t *testing.T) {
+	s := newSpace()
+	data := strings.Join([]string{".#.", "..#", "###"}, "\n")
+	s.loadSlice(data, 0)
+	t.Log("Before:")
+	t.Log(s.dump())
+
+	want := newSpace()
+	want.loadSlice(strings.Join([]string{
+		"#..",
+		"..#",
+		".#."}, "\n"), -1)
+	want.loadSlice(strings.Join([]string{
+		"#.#",
+		".##",
+		".#."}, "\n"), 0)
+	want.loadSlice(strings.Join([]string{
+		"#..",
+		"..#",
+		".#."}, "\n"), 1)
+	s.evolve()
+	t.Log("After:")
+	t.Log(s.dump())
+	if diff := cmp.Diff(want.dump(), s.dump()); diff != "" {
+		t.Errorf("bad evolution: %s", diff)
+	}
+
+}
