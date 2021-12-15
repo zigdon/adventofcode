91e8357c9dc2f0fe6de60cdbd515bf786de9977f
Dan Boger <dan@peeron.com>
2021-12-14 20:32:17 -0800

    Only kinda redid the entire thing, but it ran part B in less than a second.

--- 2021/14/14.go
+++ 2021/14/14.go
@@ -9,6 +9,86 @@ import (
 	"github.com/zigdon/adventofcode/common"
 )
 
+type transformation struct {
+	Id    string
+	Delta int
+}
+
+type polymer struct {
+	Template        string
+	Rules           map[string]string
+	Histogram       map[string]int64
+	Transformations map[string][]transformation
+}
+
+func newPolymer(tmpl string, rules map[string]string) *polymer {
+	p := &polymer{
+		Template:        tmpl,
+		Rules:           rules,
+		Histogram:       make(map[string]int64),
+		Transformations: make(map[string][]transformation),
+	}
+
+	// Count individual letters
+	for _, c := range tmpl {
+		p.Histogram[string(c)]++
+	}
+
+	// Count pairs
+	for i := 0; i < len(tmpl)-1; i++ {
+		p.Histogram[fmt.Sprintf("%c%c", tmpl[i], tmpl[i+1])]++
+	}
+
+	// Each rule takes AB -> ACB. This means we remove one AB, add one each of
+	// AC, CB, and add one C.
+	for k, v := range rules {
+		p.Transformations[k] = []transformation{
+			{k, -1},
+			{fmt.Sprintf("%c%s", k[0], v), 1},
+			{fmt.Sprintf("%s%c", v, k[1]), 1},
+			{v, 1},
+		}
+	}
+
+	return p
+}
+
+func (p *polymer) process() {
+	// iterate on all the histograms with values > 0
+	// apply transformations into a new histogram
+	// replace original with new
+	nh := make(map[string]int64)
+	for k, v := range p.Histogram {
+		if v == 0 {
+			continue
+		}
+		if len(k) == 1 { // single letter, just copy it
+			nh[k] += v
+			continue
+		}
+
+		ts, ok := p.Transformations[k]
+		if !ok {
+			log.Printf("no transformation for %s", k)
+			continue
+		}
+		nh[k] += v
+		for _, t := range ts {
+			nh[t.Id] += int64(t.Delta) * v
+		}
+	}
+
+	for k, v := range nh {
+		if v == 0 {
+			delete(nh, k)
+		}
+	}
+
+	// log.Printf("%v -> %v\n%s", p.Histogram, nh, cmp.Diff(p.Histogram, nh))
+
+	p.Histogram = nh
+}
+
 func readFile(path string) (string, map[string]string) {
 	data := common.ReadTransformedFile(
 		path,
@@ -68,6 +148,7 @@ func histogram(polymer string) []bucket {
 
 func main() {
 	template, rules := readFile("input.txt")
+	p := newPolymer(template, rules)
 	for i := 0; i < 10; i++ {
 		template = process(template, rules)
 		log.Printf("After step %d: %d", i, len(template))
@@ -75,4 +156,27 @@ func main() {
 	hist := histogram(template)
 	l := len(hist) - 1
 	log.Printf("%d - %d = %d", hist[0].Count, hist[l].Count, hist[0].Count-hist[l].Count)
+
+	// part 2
+	for i := 0; i < 40; i++ {
+		log.Printf("after step %d", i)
+		p.process()
+	}
+
+	var minC, maxC int64
+	var minR, maxR string
+	for k, v := range p.Histogram {
+		if len(k) != 1 {
+			continue
+		}
+		if minC == 0 || v < minC {
+			minC = v
+			minR = k
+		}
+		if v > maxC {
+			maxC = v
+			maxR = k
+		}
+	}
+	log.Printf("%d(%s) - %d(%s) = %d", maxC, maxR, minC, minR, maxC-minC)
 }
--- 2021/14/14_test.go
+++ 2021/14/14_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -47,11 +48,27 @@ func TestProcess(t *testing.T) {
 
 	template, rules := readFile("sample.txt")
 	for i, w := range want {
-		t.Logf("starting %d: %s", i, template)
 		template = process(template, rules)
-		t.Logf(" ->       : %s", template)
 		if template != w {
 			t.Errorf("bad polymer after step %d:\nwant: %q\n got: %q", i, w, template)
+		}
+	}
+}
+
+func TestPolymerProcess(t *testing.T) {
+	want := []string{
+		"NCNBCHB",
+		"NBCCNBBBCBHCB",
+		"NBBBCNCCNBBNBNBBCHBHHBCHB",
+		"NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB",
+	}
+
+	template, rules := readFile("sample.txt")
+	p := newPolymer(template, rules)
+	for i, w := range want {
+		p.process()
+		if diff := cmp.Diff(newPolymer(w, nil).Histogram, p.Histogram); diff != "" {
+			t.Errorf("bad polymer after step %d: wanted %q\n%s", i, w, diff)
 		}
 	}
 }
@@ -68,5 +85,47 @@ func TestHistogram(t *testing.T) {
 	hist := histogram(template)
 	if diff := cmp.Diff([]bucket{{'B', 1749}, {'H', 161}}, []bucket{hist[0], hist[len(hist)-1]}); diff != "" {
 		t.Errorf("bad histogram:\n%s", diff)
+	}
+}
+
+func TestPolymer(t *testing.T) {
+	tmpl, rules := readFile("sample.txt")
+	got := newPolymer(tmpl, rules)
+	want := &polymer{
+		Template: tmpl,
+		Rules:    rules,
+		Histogram: map[string]int{
+			"N": 2, "C": 1, "B": 1,
+			"NN": 1, "NC": 1, "CB": 1,
+		},
+	}
+
+	ignoreTransformations := cmp.FilterPath(
+		func(p cmp.Path) bool {
+			return p.String() == "Transformations"
+		}, cmp.Ignore())
+
+	if diff := cmp.Diff(want, got, ignoreTransformations); diff != "" {
+		t.Errorf("bad polymer:\n%s", diff)
+	}
+
+	// Spot check some transformations
+
+	wantTransformations := map[string][]transformation{
+		"CH": {{"CH", -1}, {"B", 1}, {"CB", 1}, {"BH", 1}},
+		"BC": {{"BC", -1}, {"B", 1}, {"BB", 1}, {"BC", 1}},
+	}
+
+	for k, v := range wantTransformations {
+		gt := got.Transformations[k]
+		sort.Slice(gt, func(i, j int) bool {
+			return gt[i].Id < gt[j].Id
+		})
+		sort.Slice(v, func(i, j int) bool {
+			return v[i].Id < v[j].Id
+		})
+		if diff := cmp.Diff(v, gt); diff != "" {
+			t.Errorf("bad (%s) transformations:\n%s", k, diff)
+		}
 	}
 }
