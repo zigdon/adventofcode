1b11415566a70c6f9e7aa8d6a36a96395bc2feb6
Dan Boger <dan@peeron.com>
2021-12-15 22:36:27 -0800

    Implement A*, and it all goes quick, day 15 done.

--- 2021/15/15.go
+++ 2021/15/15.go
@@ -1,28 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"time"
+	"math"
+	"strings"
 
 	"github.com/zigdon/adventofcode/common"
 )
+
+// Attempting to implement https://en.wikipedia.org/wiki/A*_search_algorithm
 
 type coord struct {
 	X, Y int
 }
 
-type board struct {
-	Danger   [][]int
-	Exit     coord
-	Best     int
-	Attempt  *trail
-	Solution *trail
+func (c coord) neighbours() []coord {
+	return []coord{
+		{c.X, c.Y + 1},
+		{c.X, c.Y - 1},
+		{c.X + 1, c.Y},
+		{c.X - 1, c.Y},
+	}
 }
 
-type step struct {
-	Pos    coord
-	Danger int
-	Seen   map[coord]bool
+type board struct {
+	Danger [][]int
+	Exit   coord
+}
+
+func (b *board) duplicate(n int) {
+	nb := [][]int{}
+	for y := range b.Danger {
+		line := []int{}
+		for i := 0; i < n; i++ {
+			for _, n := range b.Danger[y] {
+				d := n + i
+				if d > 9 {
+					d -= 9
+				}
+				line = append(line, d)
+			}
+		}
+		nb = append(nb, line)
+	}
+
+	lnb := len(nb)
+	for i := 1; i < n; i++ {
+		for y := 0; y < lnb; y++ {
+			line := []int{}
+			for _, n := range nb[y] {
+				d := n + i
+				if d > 9 {
+					d -= 9
+				}
+				line = append(line, d)
+			}
+			nb = append(nb, line)
+		}
+	}
+
+	b.Danger = nb
+	b.Exit.X = len(b.Danger[0]) - 1
+	b.Exit.Y = len(b.Danger) - 1
 }
 
 type dir int
@@ -38,133 +78,161 @@ func (d dir) String() string {
 	return []string{"->", "vv", "<-", "^^"}[d]
 }
 
-func move(cur *step, d dir) *step {
-	next := &step{
-		Pos:  cur.Pos,
-		Seen: make(map[coord]bool),
-	}
-	switch d {
-	case RIGHT:
-		next.Pos.X++
-	case LEFT:
-		next.Pos.X--
-	case UP:
-		next.Pos.Y--
-	case DOWN:
-		next.Pos.Y++
-	}
-
-	for k, v := range cur.Seen {
-		next.Seen[k] = v
-	}
-	next.Seen[next.Pos] = true
-
-	return next
+type queue struct {
+	CoordToPriority map[coord]int
+	PriorityToCoord map[int][]coord
+	Count           int
+	Lowest          int
 }
 
-type trail struct {
-	Steps []*step
+func newQueue() *queue {
+	return &queue{
+		CoordToPriority: make(map[coord]int),
+		PriorityToCoord: make(map[int][]coord),
+	}
 }
 
-func (b board) valid(s *step) bool {
-	if s.Pos.X < 0 {
-		return false
+func (q *queue) add(c coord, p int) {
+	_, ok := q.CoordToPriority[c]
+	if ok {
+		return
 	}
-	if s.Pos.Y < 0 {
-		return false
+	q.CoordToPriority[c] = p
+	q.PriorityToCoord[p] = append(q.PriorityToCoord[p], c)
+	q.Count++
+	if q.Lowest == 0 || p < q.Lowest {
+		q.Lowest = p
 	}
-	if s.Pos.X > b.Exit.X {
-		return false
-	}
-	if s.Pos.Y > b.Exit.Y {
-		return false
-	}
-
-	return true
 }
 
-func (b board) guess() *step {
-	// Look at the last step in the trail, pick the next step we haven't tried before
-	// Mark it as tried in the current step, create a new step, add it to the trail, and return it.
-	// If nothing left to try, return nil
-	cur := b.Attempt.Steps[len(b.Attempt.Steps)-1]
-	for _, d := range []dir{RIGHT, DOWN, LEFT, UP} {
-		if n := move(cur, d); !cur.Seen[n.Pos] && b.valid(n) {
-			n.Danger = cur.Danger + b.Danger[n.Pos.Y][n.Pos.X]
-			return n
-		}
-	}
-	return nil
+func (q *queue) isEmpty() bool {
+	return q.Count == 0
 }
 
-func (b board) backtrack() bool {
-	attempts := len(b.Attempt.Steps)
-	if attempts > 1 {
-		b.Attempt.Steps = b.Attempt.Steps[:attempts-1]
-		return true
+func (q *queue) getLowest() (coord, error) {
+	if q.isEmpty() {
+		return coord{0, 0}, fmt.Errorf("empty queue")
 	}
-	return false
-}
 
-func (b board) solve(x, y int) (*trail, int) {
-	// - at each position, try RDLU (avoiding locations you've been to), add the danger
-	// - if the danger is higher than best-case, pop
-	lastPrint := time.Now()
-	count := int64(0)
-	for {
-		count++
-		cur := b.Attempt.Steps[len(b.Attempt.Steps)-1]
-		next := b.guess()
-		if next == nil {
-			if time.Now().Sub(lastPrint) > 10*time.Second {
-				draw(b.Attempt, b.Exit)
-				lastPrint = time.Now()
-				log.Printf("guesses: %d", count)
+	c := q.PriorityToCoord[q.Lowest][0]
+	q.Count--
+	if len(q.PriorityToCoord[q.Lowest]) > 1 {
+		q.PriorityToCoord[q.Lowest] = q.PriorityToCoord[q.Lowest][1:]
+	} else {
+		delete(q.PriorityToCoord, q.Lowest)
+		q.Lowest = 0
+		for p, cs := range q.PriorityToCoord {
+			if len(cs) == 0 {
+				continue
 			}
-			if !b.backtrack() {
-				break
-			}
-			continue
-		}
-		cur.Seen[next.Pos] = true
-		if b.Best != 0 && next.Danger > b.Best {
-			continue
-		}
-		b.Attempt.Steps = append(b.Attempt.Steps, next)
-		if next.Pos == b.Exit {
-			b.Best = next.Danger
-			sol := &trail{}
-			for _, s := range b.Attempt.Steps {
-				sol.Steps = append(sol.Steps, &step{Pos: s.Pos, Danger: s.Danger})
-			}
-			b.Solution = sol
-			log.Printf("Got to the exit (%d steps, %d danger)!", len(sol.Steps), b.Best)
-			draw(sol, b.Exit)
-			if !b.backtrack() {
-				break
+			if q.Lowest == 0 || p < q.Lowest {
+				q.Lowest = p
 			}
 		}
 	}
 
-	draw(b.Solution, b.Exit)
-	return b.Solution, b.Best
+	return c, nil
 }
 
-func draw(t *trail, exit coord) {
-	m := make(map[coord]bool)
-	for _, s := range t.Steps {
-		m[s.Pos] = true
+func (q *queue) has(c coord) bool {
+	_, ok := q.CoordToPriority[c]
+	return ok
+}
+
+type cameFrom struct {
+	Src  map[coord]coord
+	Cost map[coord]int
+}
+
+func (cf cameFrom) getSrc(c coord) coord {
+	return cf.Src[c]
+}
+
+func (cf cameFrom) getScore(c coord) int {
+	return cf.Cost[c]
+}
+
+func (cf cameFrom) set(src, dst coord, score int) {
+	cf.Cost[dst] = score
+	cf.Src[dst] = src
+}
+
+func (cf cameFrom) path(c coord) []coord {
+	log.Printf("getting path ending at %v", c)
+	out := []coord{c}
+	for c.X != 0 && c.Y != 0 {
+		c = cf.getSrc(c)
+		log.Printf(" -> %v", c)
+		out = append(out, c)
 	}
-	for y := 0; y <= exit.Y; y++ {
-		line := ""
-		for x := 0; x <= exit.X; x++ {
-			if m[coord{x, y}] {
-				line += "#"
-			} else {
-				line += "."
+
+	return out
+}
+
+func (b *board) aStar() ([]coord, int) {
+	h := func(c coord) int {
+		return int(math.Abs(float64(b.Exit.Y-c.Y)) + math.Abs(float64(b.Exit.X-c.X)))
+	}
+
+	q := newQueue()
+	start := coord{0, 0}
+	q.add(start, h(start))
+
+	cf := cameFrom{
+		Src:  map[coord]coord{},
+		Cost: map[coord]int{{0, 0}: 0},
+	}
+
+	for !q.isEmpty() {
+		current, err := q.getLowest()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if current.X == b.Exit.X && current.Y == b.Exit.Y {
+			return cf.path(current), cf.getScore(b.Exit)
+		}
+
+		for _, c := range current.neighbours() {
+			if c.Y < 0 || c.X < 0 || c.Y > b.Exit.Y || c.X > b.Exit.X {
+				continue
+			}
+			tScore := cf.getScore(current) + b.Danger[c.Y][c.X]
+			if c.X == 0 && c.Y == 0 {
+				continue
+			}
+			if cf.getScore(c) == 0 || tScore < cf.getScore(c) {
+				cf.set(current, c, tScore)
+			}
+			if !q.has(c) {
+				q.add(c, tScore+h(c))
 			}
 		}
-		log.Print(line)
+	}
+
+	log.Print("ran out of ideas:")
+	for to, from := range cf.Src {
+		log.Printf(" %v(%d) -> %v(%d)", from, cf.Cost[from], to, cf.Cost[to])
+	}
+
+	return nil, 0
+}
+
+func (b *board) draw(p []coord) {
+	out := [][]string{}
+	for y := 0; y <= b.Exit.Y; y++ {
+		line := []string{}
+		for x := 0; x <= b.Exit.X; x++ {
+			line = append(line, ".")
+		}
+		out = append(out, line)
+	}
+
+	for _, s := range p {
+		out[s.Y][s.X] = "#"
+	}
+
+	for _, l := range out {
+		log.Print(strings.Join(l, " "))
 	}
 }
 
@@ -175,22 +243,21 @@ func readFile(path string) board {
 		common.Split(""),
 	))
 
-	start := &step{
-		Pos:  coord{0, 0},
-		Seen: map[coord]bool{{0, 0}: true},
-	}
 	return board{
 		Danger: data,
 		Exit:   coord{len(data[0]) - 1, len(data) - 1},
-		Attempt: &trail{
-			Steps: []*step{start},
-		},
 	}
 }
 
 func main() {
 	b := readFile("input.txt")
-	p, sc := b.solve(0, 0)
-	draw(p, b.Exit)
-	log.Printf("Danger = %d", sc)
+	p, s := b.aStar()
+	log.Printf("%v", p)
+	log.Printf("score: %d", s)
+
+	b = readFile("input.txt")
+	b.duplicate(5)
+	p, s = b.aStar()
+	log.Printf("%v", p)
+	log.Printf("other score: %d", s)
 }
--- 2021/15/15_test.go
+++ 2021/15/15_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -22,12 +21,6 @@ func TestReadFile(t *testing.T) {
 			{1, 2, 9, 3, 1, 3, 8, 5, 2, 1},
 			{2, 3, 1, 1, 9, 4, 4, 5, 8, 1},
 		},
-		Best: 0,
-		Attempt: &trail{
-			Steps: []*step{
-				{Seen: map[coord]bool{{0, 0}: true}},
-			},
-		},
 		Exit: coord{9, 9},
 	}
 
@@ -36,128 +29,56 @@ func TestReadFile(t *testing.T) {
 	}
 }
 
+func TestDuplicate(t *testing.T) {
+	data := readFile("sample.txt")
+	data.duplicate(2)
+	want := [][]int{
+		{1, 1, 6, 3, 7, 5, 1, 7, 4, 2, 2, 2, 7, 4, 8, 6, 2, 8, 5, 3},
+		{1, 3, 8, 1, 3, 7, 3, 6, 7, 2, 2, 4, 9, 2, 4, 8, 4, 7, 8, 3},
+		{2, 1, 3, 6, 5, 1, 1, 3, 2, 8, 3, 2, 4, 7, 6, 2, 2, 4, 3, 9},
+		{3, 6, 9, 4, 9, 3, 1, 5, 6, 9, 4, 7, 1, 5, 1, 4, 2, 6, 7, 1},
+		{7, 4, 6, 3, 4, 1, 7, 1, 1, 1, 8, 5, 7, 4, 5, 2, 8, 2, 2, 2},
+		{1, 3, 1, 9, 1, 2, 8, 1, 3, 7, 2, 4, 2, 1, 2, 3, 9, 2, 4, 8},
+		{1, 3, 5, 9, 9, 1, 2, 4, 2, 1, 2, 4, 6, 1, 1, 2, 3, 5, 3, 2},
+		{3, 1, 2, 5, 4, 2, 1, 6, 3, 9, 4, 2, 3, 6, 5, 3, 2, 7, 4, 1},
+		{1, 2, 9, 3, 1, 3, 8, 5, 2, 1, 2, 3, 1, 4, 2, 4, 9, 6, 3, 2},
+		{2, 3, 1, 1, 9, 4, 4, 5, 8, 1, 3, 4, 2, 2, 1, 5, 5, 6, 9, 2},
+		{2, 2, 7, 4, 8, 6, 2, 8, 5, 3, 3, 3, 8, 5, 9, 7, 3, 9, 6, 4},
+		{2, 4, 9, 2, 4, 8, 4, 7, 8, 3, 3, 5, 1, 3, 5, 9, 5, 8, 9, 4},
+		{3, 2, 4, 7, 6, 2, 2, 4, 3, 9, 4, 3, 5, 8, 7, 3, 3, 5, 4, 1},
+		{4, 7, 1, 5, 1, 4, 2, 6, 7, 1, 5, 8, 2, 6, 2, 5, 3, 7, 8, 2},
+		{8, 5, 7, 4, 5, 2, 8, 2, 2, 2, 9, 6, 8, 5, 6, 3, 9, 3, 3, 3},
+		{2, 4, 2, 1, 2, 3, 9, 2, 4, 8, 3, 5, 3, 2, 3, 4, 1, 3, 5, 9},
+		{2, 4, 6, 1, 1, 2, 3, 5, 3, 2, 3, 5, 7, 2, 2, 3, 4, 6, 4, 3},
+		{4, 2, 3, 6, 5, 3, 2, 7, 4, 1, 5, 3, 4, 7, 6, 4, 3, 8, 5, 2},
+		{2, 3, 1, 4, 2, 4, 9, 6, 3, 2, 3, 4, 2, 5, 3, 5, 1, 7, 4, 3},
+		{3, 4, 2, 2, 1, 5, 5, 6, 9, 2, 4, 5, 3, 3, 2, 6, 6, 7, 1, 3},
+	}
+	if diff := cmp.Diff(want, data.Danger); diff != "" {
+		for _, l := range data.Danger {
+			t.Logf("%v", l)
+		}
+		t.Errorf("bad duplicate:\n%s", diff)
+	}
+}
+
 func TestFindPath(t *testing.T) {
 	b := readFile("sample.txt")
-	_, score := b.solve(0, 0)
+	p, score := b.aStar()
+	t.Logf("%v", p)
+	b.draw(p)
 	if score != 40 {
 		t.Errorf("bad score, wanted 40, got %d", score)
 	}
 }
 
-func TestMove(t *testing.T) {
-	tests := []struct {
-		d    dir
-		want coord
-	}{
-		{
-			d:    UP,
-			want: coord{5, 4},
-		},
-		{
-			d:    DOWN,
-			want: coord{5, 6},
-		},
-		{
-			d:    LEFT,
-			want: coord{4, 5},
-		},
-		{
-			d:    RIGHT,
-			want: coord{6, 5},
-		},
-	}
-
-	start := &step{
-		Pos: coord{5, 5},
-		Seen: map[coord]bool{
-			{5, 5}: true,
-			{4, 5}: true,
-			{3, 5}: true,
-		},
-	}
-	for _, tc := range tests {
-		t.Run(fmt.Sprintf("%s", tc.d), func(t *testing.T) {
-			got := move(start, tc.d)
-			if cmp.Diff(tc.want, got.Pos) != "" {
-				t.Errorf("bad move: want %v, got %v", tc.want, got.Pos)
-			}
-			for k := range start.Seen {
-				if !got.Seen[k] {
-					t.Errorf("seen not copied properly, missing %v", k)
-				}
-			}
-			if !got.Seen[got.Pos] {
-				t.Error("move not marked!")
-			}
-		})
-	}
-}
-
-func TestGuess(t *testing.T) {
-	tests := []struct {
-		desc string
-		in   *step
-		want *step
-	}{
-		{
-			desc: "normal move",
-			in: &step{
-				Pos:    coord{1, 1},
-				Danger: 5,
-				Seen:   map[coord]bool{{1, 1}: true},
-			},
-			want: &step{
-				Pos:    coord{2, 1},
-				Danger: 13,
-				Seen:   map[coord]bool{{1, 1}: true, {2, 1}: true},
-			},
-		},
-		{
-			desc: "avoid repeating moves",
-			in: &step{
-				Pos:    coord{1, 1},
-				Danger: 5,
-				Seen:   map[coord]bool{{1, 1}: true, {2, 1}: true},
-			},
-			want: &step{
-				Pos:    coord{1, 2},
-				Danger: 6,
-				Seen:   map[coord]bool{{1, 1}: true, {2, 1}: true, {1, 2}: true},
-			},
-		},
-		{
-			desc: "no move",
-			in: &step{
-				Pos:    coord{1, 1},
-				Danger: 23,
-				Seen: map[coord]bool{
-					{1, 1}: true,
-					{0, 1}: true,
-					{1, 0}: true,
-					{2, 1}: true,
-					{1, 2}: true,
-				},
-			},
-		},
-	}
-
-	b := board{
-		Danger: [][]int{
-			{1, 1, 6},
-			{1, 3, 8},
-			{2, 1, 3},
-		},
-		Best: 20,
-		Exit: coord{2, 2},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.desc, func(t *testing.T) {
-			b.Attempt = &trail{Steps: []*step{tc.in}}
-			got := b.guess()
-			if diff := cmp.Diff(tc.want, got); diff != "" {
-				t.Errorf("bad guess:\n%s", diff)
-			}
-		})
+func TestFindPathDup(t *testing.T) {
+	b := readFile("sample.txt")
+	b.duplicate(5)
+	p, score := b.aStar()
+	t.Logf("%v", p)
+	b.draw(p)
+	if score != 315 {
+		t.Errorf("bad score, wanted 315, got %d", score)
 	}
 }
