baa4ced2b1f06f39fa4ee39e9ec19d7d5e660092
Dan Boger <dan@peeron.com>
2022-12-14 12:31:38 -0800

    14a

---
+++ 2022/14/14.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/zigdon/adventofcode/common"
+)
+
+type Point struct {
+	X, Y int
+}
+
+func NewPoint(s string) Point {
+	ns := strings.Split(s, ",")
+	return Point{common.MustInt(ns[0]), common.MustInt(ns[1])}
+}
+func (p Point) String() string {
+	return fmt.Sprintf("[%d,%d]", p.X, p.Y)
+}
+
+type Object int
+
+const (
+	Air Object = iota
+	Rock
+	Sand
+	Source
+)
+
+func (o Object) String() string {
+	switch o {
+	case Air:
+		return "."
+	case Rock:
+		return "#"
+	case Sand:
+		return "o"
+	case Source:
+		return "+"
+	default:
+		return "."
+	}
+}
+
+type Cave struct {
+	Scan     map[Point]Object
+	Min, Max *Point
+}
+
+func NewCave() *Cave {
+	return &Cave{
+		Scan: make(map[Point]Object),
+	}
+}
+func (c *Cave) String() string {
+	res := []string{}
+	for y := c.Min.Y; y <= c.Max.Y; y++ {
+		l := fmt.Sprintf("%3d: ", y)
+		for x := c.Min.X; x <= c.Max.X; x++ {
+			l += c.Scan[Point{x, y}].String()
+		}
+		res = append(res, l)
+	}
+
+	return strings.Join(res, "\n")
+}
+func (c *Cave) UpdateSize(p Point) {
+	if c.Min == nil {
+		c.Min = &Point{p.X, p.Y}
+	} else {
+		if p.X < c.Min.X {
+			c.Min.X = p.X
+		}
+		if p.Y < c.Min.Y {
+			c.Min.Y = p.Y
+		}
+	}
+	if c.Max == nil {
+		c.Max = &Point{p.X, p.Y}
+	} else {
+		if p.X > c.Max.X {
+			c.Max.X = p.X
+		}
+		if p.Y > c.Max.Y {
+			c.Max.Y = p.Y
+		}
+	}
+}
+func (c *Cave) AddLine(a, b Point) {
+	if b.X < a.X || b.Y < a.Y {
+		a, b = b, a
+	}
+	c.UpdateSize(a)
+	c.UpdateSize(b)
+	if a.X == b.X {
+		for y := a.Y; y <= b.Y; y++ {
+			c.Scan[Point{a.X, y}] = Rock
+		}
+	} else {
+		for x := a.X; x <= b.X; x++ {
+			c.Scan[Point{x, a.Y}] = Rock
+		}
+	}
+}
+func (c *Cave) AddStructure(l string) {
+	ps := strings.Split(l, " -> ")
+	var cur Point
+	for n, coord := range ps {
+		p := NewPoint(coord)
+		if n == 0 {
+			cur = p
+			continue
+		}
+		c.AddLine(cur, p)
+		cur = p
+	}
+}
+func (c *Cave) Drop(p Point) *Point {
+	pos := &Point{p.X, p.Y}
+	// Starting from the drop point, go down until you hit something, then
+	// down-left, then down-right, then stop.
+	for pos.Y <= c.Max.Y {
+		pos.Y += 1
+		if c.Scan[*pos] == Air {
+			continue
+		}
+		pos.X -= 1
+		if c.Scan[*pos] == Air {
+			continue
+		}
+		pos.X += 2
+		if c.Scan[*pos] == Air {
+			continue
+		}
+		pos.Y -= 1
+		pos.X -= 1
+		break
+	}
+	if pos.Y > c.Max.Y {
+		return nil
+	}
+	c.Scan[*pos] = Sand
+
+	return pos
+}
+
+func one(c *Cave) int {
+	start := Point{500, 0}
+	n := 0
+	for true {
+		got := c.Drop(start)
+		if got == nil {
+			return n
+		}
+		n++
+	}
+	return 0
+}
+
+func two(data *Cave) int {
+	return 0
+}
+
+func readFile(path string) *Cave {
+	res := common.AsStrings(common.ReadTransformedFile(path, common.IgnoreBlankLines))
+	c := NewCave()
+	for _, l := range res {
+		c.AddStructure(l)
+	}
+	source := Point{500, 0}
+	c.Scan[source] = Source
+	c.UpdateSize(source)
+
+	return c
+}
+
+func main() {
+	log.Println("Reading data...")
+	data := readFile(os.Args[1])
+
+	log.Println("Part A")
+	res := one(data)
+	fmt.Printf("%v\n", res)
+
+	log.Println("Part B")
+	data = readFile(os.Args[1])
+	res = two(data)
+	fmt.Printf("%v\n", res)
+}
---
+++ 2022/14/14_test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestReadFile(t *testing.T) {
+	got := readFile("sample.txt")
+	want := &Cave{
+		Min: &Point{494, 0},
+		Max: &Point{503, 9},
+		Scan: map[Point]Object{
+			{494, 9}: Rock,
+			{495, 9}: Rock,
+			{496, 9}: Rock,
+			{497, 9}: Rock,
+			{498, 9}: Rock,
+			{499, 9}: Rock,
+			{500, 9}: Rock,
+			{501, 9}: Rock,
+			{502, 9}: Rock,
+			{502, 8}: Rock,
+			{502, 7}: Rock,
+			{502, 6}: Rock,
+			{502, 5}: Rock,
+			{502, 4}: Rock,
+			{503, 4}: Rock,
+			{496, 6}: Rock,
+			{497, 6}: Rock,
+			{498, 6}: Rock,
+			{498, 5}: Rock,
+			{498, 4}: Rock,
+			{500, 0}: Source,
+		},
+	}
+
+	t.Logf("\n%s", got)
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestDrop(t *testing.T) {
+	cave := readFile("sample.txt")
+	tests := []*Point{
+		{500, 8},
+		{499, 8},
+		{501, 8},
+		{500, 7},
+	}
+
+	source := Point{500, 0}
+	for n, tc := range tests {
+		got := cave.Drop(source)
+		if cmp.Diff(tc, got) != "" {
+			t.Errorf("Error after %d drops: want %s, got %s\n%s", n, tc, got, cave)
+		}
+	}
+
+	for n := len(tests); n < 24; n++ {
+		if cave.Drop(source) == nil {
+			t.Errorf("Drop %d is off the ledge", n)
+		}
+	}
+	p := cave.Drop(source)
+	if p != nil {
+		t.Errorf("Last drop ended up at %s\n%s", p, cave)
+	}
+	t.Log(cave)
+}
+
+func TestOne(t *testing.T) {
+	data := readFile("sample.txt")
+	got := one(data)
+	want := 24
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf(diff)
+	}
+}
+
+func TestTwo(t *testing.T) {
+	data := readFile("sample.txt")
+	got := two(data)
+	want := 0
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf(diff)
+	}
+}
