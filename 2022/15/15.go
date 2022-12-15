16cdcde16ace7b792f0acc39a5b26d0f330de1bc
Dan Boger <dan@peeron.com>
2022-12-15 13:49:10 -0800

    15a: now with actually noticing the size of the input

--- 2022/15/15.go
+++ 2022/15/15.go
@@ -34,14 +34,38 @@ func (o Object) String() string {
 	return []string{"?", ".", "S", "B"}[o]
 }
 
+type Range struct {
+	From, To int
+}
+
+func (r *Range) String() string {
+	return fmt.Sprintf("R[%d:%d]", r.From, r.To)
+}
+func (r *Range) Contains(x int) bool {
+	return x >= r.From && x <= r.To
+}
+func (r *Range) Merge(r2 *Range) bool {
+	if r.To+1 < r2.From || r.From-1 > r2.To {
+		return false
+	}
+	r.From = int(math.Min(float64(r.From), float64(r2.From)))
+	r.To = int(math.Max(float64(r.To), float64(r2.To)))
+	return true
+}
+func (r *Range) Copy() *Range {
+	return &Range{r.From, r.To}
+}
+
 type Field struct {
 	Objs     map[Point]Object
 	Min, Max *Point
+	Gaps     map[int][]*Range
 }
 
 func NewField() *Field {
 	return &Field{
 		Objs: make(map[Point]Object),
+		Gaps: make(map[int][]*Range),
 	}
 }
 func (f *Field) String() string {
@@ -51,43 +75,28 @@ func (f *Field) String() string {
 	}
 	for y := f.Min.Y; y <= f.Max.Y; y++ {
 		l := fmt.Sprintf("%3d: ", y)
+		rs := MergeRanges(f.Gaps[y])
 		for x := f.Min.X; x <= f.Max.X; x++ {
-			l += f.Objs[Point{x, y}].String()
+			o, ok := f.Objs[Point{x, y}]
+			if ok {
+				l += o.String()
+				continue
+			}
+			found := false
+			for _, r := range rs {
+				if r.Contains(x) {
+					l += "#"
+					found = true
+					break
+				}
+			}
+			if !found {
+				l += "."
+			}
 		}
 		res = append(res, l)
 	}
 	return strings.Join(res, "\n")
-}
-func (f *Field) Fill() {
-	for y := f.Min.Y; y <= f.Max.Y; y++ {
-		for x := f.Min.X; x <= f.Max.X; x++ {
-			p := Point{x, y}
-			if _, ok := f.Objs[p]; !ok {
-				f.Objs[p] = Unknown
-			}
-		}
-	}
-}
-func (f *Field) ParseMap(o Point, m string) {
-	for i, l := range strings.Split(m, "\n") {
-		y := o.Y + i
-		for j, r := range l {
-			x := o.X + j
-			p := Point{x, y}
-			var obj Object
-			switch r {
-			case '.':
-				obj = Unknown
-			case '#':
-				obj = Air
-			case 'S':
-				obj = Sensor
-			case 'B':
-				obj = Beacon
-			}
-			f.Objs[p] = obj
-		}
-	}
 }
 func (f *Field) UpdateSize(p Point) {
 	if f.Min == nil {
@@ -109,23 +118,18 @@ func (f *Field) UpdateSize(p Point) {
 		f.Max.Y = p.Y
 	}
 }
-func (f *Field) FillAir(p Point, r int) {
-	if r > 1 {
-		f.FillAir(p, r-1)
-	}
+func (f *Field) FillSparseAir(p Point, r int) {
 	f.UpdateSize(Point{p.X + r, p.Y + r})
 	f.UpdateSize(Point{p.X - r, p.Y - r})
-	mkAir := func(p Point) {
-		if f.Objs[p] != Unknown {
-			return
-		}
-		f.Objs[p] = Air
-	}
 	for i := 0; i <= r; i++ {
-		mkAir(Point{p.X + i, p.Y + r - i})
-		mkAir(Point{p.X - i, p.Y + r - i})
-		mkAir(Point{p.X + i, p.Y - r + i})
-		mkAir(Point{p.X - i, p.Y - r + i})
+		rng := &Range{p.X + i - r, p.X + r - i}
+		for _, y := range []int{p.Y + i, p.Y - i} {
+			if f.Gaps[y] == nil {
+				f.Gaps[y] = []*Range{rng.Copy()}
+			} else {
+				f.Gaps[y] = MergeRanges(append(f.Gaps[y], rng))
+			}
+		}
 	}
 }
 func (f *Field) AddSensor(s, b Point) {
@@ -134,18 +138,34 @@ func (f *Field) AddSensor(s, b Point) {
 	log.Printf("New sensor+beacon: S:%s B:%s dist=%d", s, b, dist)
 	f.UpdateSize(s)
 	f.UpdateSize(b)
-	f.FillAir(s, dist)
+	f.FillSparseAir(s, dist)
 
 	f.Objs[s] = Sensor
 	f.Objs[b] = Beacon
 }
 
+func MergeRanges(gaps []*Range) []*Range {
+	rs := []*Range{}
+	for _, g := range gaps {
+		found := false
+		for _, r := range rs {
+			if r.Merge(g) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			rs = append(rs, g.Copy())
+		}
+	}
+	return rs
+}
+
 func one(f *Field, y int) int {
 	res := 0
-	for x := f.Min.X; x <= f.Max.X; x++ {
-		if o, ok := f.Objs[Point{x, y}]; ok && o == Air {
-			res++
-		}
+	rs := MergeRanges(f.Gaps[y])
+	for _, r := range rs {
+		res += r.To - r.From
 	}
 	return res
 }
